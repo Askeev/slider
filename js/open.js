94c6ad990ec6f7f095d4574c3e3d3fc4d443e515
Askeev <askeev9601@gmail.com>
2024-02-12 19:42:12 +0600

    init

---
+++ js/open.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('openModal').addEventListener('click', function(){
+        document.getElementById('modalWrapper').style.display='flex'
+    })
+})
